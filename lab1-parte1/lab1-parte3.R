adc414cdd96dd2b43c4af97f541030d1143043ac
brunabarbosa <bruna.barbosa@ccc.ufcg.edu.br>
2017-05-23 16:52:43 -0300

    perguntas

---
+++ lab1-parte1/lab1-parte3.R
@@ -0,0 +1,10 @@
+
+# 1. Series que nao foram renovadas possuem qualidade inferior em sua ultima temporada?
+# 2. Episodios pilotos possuem qualidade inferior comparado ao resto da temporada?
+# 3. Series com grande quantidade de episodios na temporada (entre 20 e 25) possuem qualidade inferior a 
+#    series com pequena quantidade de episodios (entre 6 e 10)?
+# 4. Series com grande numero de avaliacoes do usuario tem uma maior qualidade?
+# 5. Series mais conhecidas (com grande numero de temporadas), tem maior qualidade?
+# 6. Series ja encerradas possuem uma proporcao maior de pessoas votando 10 no ultimo episodio
+# 7. Episodios pilotos possuem maior parte das pessoas atribuindo notas abaixo de 7.
+# 8. Season finales sao mais bem avaliadas que os outros episodios da serie
